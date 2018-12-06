db7a38608dccd0baeade76827279011d7310196f
Prasad Silva <prasad@precisetarget.com>
2018-12-06 14:18:44 -0500

    Day 1 completed

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// Forward declarations
+namespace day1 {
+    void problem1();
+    void problem2();
+}
+
+int main(int argc, char const *argv[])
+{
+    day1::problem1();
+    day1::problem2();
+    
+    return 0;
+}
