6e4ffe3f25ed94a8143a2df360362d94c3714781
Prasad Silva <prasad@precisetarget.com>
2018-12-06 18:13:35 -0500

    Day 2 complete

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,18 @@ namespace day1 {
     void problem1();
     void problem2();
 }
+namespace day2 {
+    void problem1();
+    void problem2();
+}
 
 int main(int argc, char const *argv[])
 {
     day1::problem1();
     day1::problem2();
-    
+
+    day2::problem1();
+    day2::problem2();
+
     return 0;
 }
