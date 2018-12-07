bfc7ccffe30926302dcf6b6cb50a0bbb9d10fbcf
Prasad Silva <prasad@precisetarget.com>
2018-12-07 11:14:54 -0500

    Refactored runner and added day3 p1

---
+++ src/day3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace day3 {
+
+    void problem1() {
+        std::cout << "Day 3 - Problem 1" << std::endl;
+    }
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <array>
+#include <vector>
+#include <functional>
 
 // Forward declarations
 namespace day1 {
@@ -9,14 +12,54 @@ namespace day2 {
     void problem1();
     void problem2();
 }
+namespace day3 {
+    void problem1();
+}
 
 int main(int argc, char const *argv[])
 {
-    day1::problem1();
-    day1::problem2();
+    std::vector<std::vector<std::function<void(void)>>> days = {
+        { day1::problem1, day1:: problem2 },
+        { day2::problem1, day2:: problem2 },
+        { day3::problem1 }
+    };
 
-    day2::problem1();
-    day2::problem2();
+    if (argc > 2) {
+        std::cerr << "ERROR: Specify no params or a day # to run a specific day" << std::endl;
+        return -1;
+    }
+
+    int dayToRun = -1;
+    if (argc == 2) {
+        try {
+            dayToRun = std::stoi(argv[1]);
+        } catch (...) {
+            std::cerr << "ERROR: Invalid parameter!" << std::endl;
+            return -2;
+        }
+        if (dayToRun < 1 || dayToRun > days.size()) {
+            std::cerr << "ERROR: Day parameter is invalid!" << std::endl;
+            return -3;
+        }
+    }
+
+    if (dayToRun != -1) {
+        // Run a specific day
+        auto& problems = days[dayToRun - 1];
+        std::cout << "Running day " << dayToRun << std::endl;
+        for (auto& problem : problems) {
+            problem();
+            std::cout << std::endl;
+        }
+    } else {
+        // Run all days
+        for (auto& problems : days) {
+            for (auto& problem : problems) {
+                problem();
+                std::cout << std::endl;
+            }
+        }
+    }
 
     return 0;
 }
