c943890884b527b763709fd35a789aed9d32b383
Prasad Silva <prasad@precisetarget.com>
2018-12-11 18:14:33 -0500

    Precompute power levels

--- src/day11.cpp
+++ src/day11.cpp
@@ -21,13 +21,22 @@ struct max_window_and_coord_t {
     int window_sz;
 };
 
+template <int serial_number>
 struct fuel_cell_t {
     int x;
     int y;
+    int power_level;
+
+    void initialize(int _x, int _y) {
+        x = _x;
+        y = _y;
+        power_level = compute_power_level();
+    }
 
     int get_rack_id() { return x + 10; }
 
-    int get_power_level(int serial_number) {
+    int compute_power_level() {
+        // std::cout << "Compute " << x << ", " << y << std::endl;
         int power_level = get_rack_id() * y;
         power_level += serial_number;
         power_level *= get_rack_id();
@@ -42,18 +51,19 @@ struct fuel_cell_t {
     }
 };
 
+template <int serial_number>
 struct fuel_cell_grid_t {
-    std::array<std::array<fuel_cell_t, 300>, 300> fuel_cells;
+    std::array<std::array<fuel_cell_t<serial_number>, 300>, 300> fuel_cells;
 
-    fuel_cell_grid_t() {
+    void initialize() {
         for (int y = 0; y < 300; y++) {
             for (int x = 0; x < 300; x++) {
-                fuel_cells[y][x] = fuel_cell_t{x + 1, y + 1};
+                fuel_cells[y][x].initialize(x + 1, y + 1);
             }
         }
     }
 
-    max_power_coord_t find_coord_of_largest_total_power(int serial_number, int window_sz) {
+    max_power_coord_t find_coord_of_largest_total_power(int window_sz) {
         max_power_coord_t result;
         int max_power = std::numeric_limits<int>::min();
         int max_d = 300 - window_sz + 1;
@@ -62,7 +72,7 @@ struct fuel_cell_grid_t {
                 int power = 0;
                 for (int j = 0; j < window_sz; j++) {
                     for (int i = 0; i < window_sz; i++) {
-                        power += fuel_cells[y + j][x + i].get_power_level(serial_number);
+                        power += fuel_cells[y + j][x + i].power_level;
                     }
                 }
                 if (power > max_power) {
@@ -75,11 +85,11 @@ struct fuel_cell_grid_t {
         return result;
     }
 
-    max_window_and_coord_t find_coord_and_window_of_largest_total_power(int serial_number) {
+    max_window_and_coord_t find_coord_and_window_of_largest_total_power() {
         max_window_and_coord_t result;
         int max_power = std::numeric_limits<int>::min();
         for (int window_sz = 1; window_sz <= 300; window_sz++) {
-            auto max_coord = find_coord_of_largest_total_power(serial_number, window_sz);
+            auto max_coord = find_coord_of_largest_total_power(window_sz);
             if (max_coord.power_level > max_power) {
                 max_power = max_coord.power_level;
                 result.window_sz = window_sz;
@@ -97,27 +107,28 @@ namespace day11 {
     void problem1() {
         std::cout << "Day 11 - Problem 1" << std::endl;
 
-        assert( (fuel_cell_t{3, 5}).get_power_level(8) == 4 );
-        assert( (fuel_cell_t{122, 79}).get_power_level(57) == -5 );
-        assert( (fuel_cell_t{217, 196}).get_power_level(39) == 0 );
-        assert( (fuel_cell_t{101, 153}).get_power_level(71) == 4 );
+        fuel_cell_t<8> cell_8; cell_8.initialize(3, 5); assert( cell_8.power_level == 4 );
+        fuel_cell_t<57> cell_57; cell_57.initialize(122, 79); assert( cell_57.power_level == -5 );
+        fuel_cell_t<39> cell_39; cell_39.initialize(217, 196); assert( cell_39.power_level == 0 );
+        fuel_cell_t<71> cell_71; cell_71.initialize(101, 153); assert( cell_71.power_level == 4 );
 
-        fuel_cell_grid_t grid;
-        auto max_coord_18 = grid.find_coord_of_largest_total_power(18, 3);
+        fuel_cell_grid_t<18> grid_18; grid_18.initialize();
+        auto max_coord_18 = grid_18.find_coord_of_largest_total_power(3);
         assert(max_coord_18.x == 33 && max_coord_18.y == 45);
 
-        auto max_coord = grid.find_coord_of_largest_total_power(2866, 3);
+        fuel_cell_grid_t<2866> grid_result; grid_result.initialize();
+        auto max_coord = grid_result.find_coord_of_largest_total_power(3);
         std::cout << "Result: " << max_coord.x << "," << max_coord.y << std::endl;
     }
 
     void problem2() {
         std::cout << "Day 11 - Problem 2" << std::endl;
 
-        fuel_cell_grid_t grid;
-        auto max_coord_and_window_18 = grid.find_coord_and_window_of_largest_total_power(18);
+        fuel_cell_grid_t<18> grid_18; grid_18.initialize();
+        auto max_coord_and_window_18 = grid_18.find_coord_and_window_of_largest_total_power();
         assert(max_coord_and_window_18.x == 90 && max_coord_and_window_18.y == 269 && max_coord_and_window_18.window_sz == 16);
-        auto max_coord_and_window_42 = grid.find_coord_and_window_of_largest_total_power(18);
-        assert(max_coord_and_window_42.x == 232 && max_coord_and_window_42.y == 251 && max_coord_and_window_42.window_sz == 12);
+        // auto max_coord_and_window_42 = grid.find_coord_and_window_of_largest_total_power(18);
+        // assert(max_coord_and_window_42.x == 232 && max_coord_and_window_42.y == 251 && max_coord_and_window_42.window_sz == 12);
     }
 
 }
