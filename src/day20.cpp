52ec35f64305cfa51099355c646ca3f3f02d82d1
Prasad Silva <prasad@bitsemble.com>
2018-12-23 19:19:05 -0500

    Day20 problem1 and problem2

--- src/day20.cpp
+++ src/day20.cpp
@@ -9,6 +9,7 @@
 #include <limits>
 #include <array>
 #include <sstream>
+#include "micropather_1_2_0/micropather.h"
 
 namespace day20 {
 
@@ -140,22 +141,86 @@ namespace day20 {
     }
   };
 
-  struct doors_t {
+  struct cell_t {
+    int x, y;
     bool valid = false;
     bool north = false, east = false, west = false, south = false;
+
+    friend std::ostream &operator<<(std::ostream &out, const cell_t &cell) {
+      out << "(" << cell.x << "," << cell.y << ") - " << (cell.valid ? "valid" : "invalid")
+        << " " << (cell.north ? "N" : "") << (cell.south ? "S" : "") << (cell.east ? "E" : "") << (cell.west ? "W" : "");
+      return out;
+    }
   };
 
   template <int map_dim>
-  struct map_t {
-    std::vector<std::vector<doors_t>> cells;
+  struct map_t : public micropather::Graph {
+    std::vector<std::vector<std::shared_ptr<cell_t>>> cells;
     int start_x = map_dim / 2, start_y = map_dim / 2;
     int bx0 = start_x, bx1 = start_x, by0 = start_y, by1 = start_y;
+    std::shared_ptr<micropather::MicroPather> pather = nullptr;
 
     void initialize() {
       cells.resize(static_cast<unsigned long>(map_dim));
-      for (auto &row : cells) {
+      for (int y = 0; y < map_dim; y++) {
+        auto &row = cells[y];
         row.resize(static_cast<unsigned long>(map_dim));
+        for (int x = 0; x < map_dim; x++) {
+          auto cell = std::make_shared<cell_t>();
+          cell->x = x;
+          cell->y = y;
+          row[x] = cell;
+        }
       }
+
+      if (!pather) {
+        pather = std::make_shared<micropather::MicroPather>(this);
+      }
+    }
+
+    // micropath::Graph interface
+    float LeastCostEstimate(void *stateStart, void *stateEnd) override {
+      auto *start = (cell_t *) stateStart;
+      auto *end = (cell_t *) stateEnd;
+      return (float) std::abs(start->x - end->x) + std::abs(start->y - end->y);
+    }
+
+    void AdjacentCost(void *state, std::vector<micropather::StateCost> *adjacent) override {
+      auto *start = (cell_t *) state;
+      int x = start->x;
+      int y = start->y;
+      if (start->north) {
+        auto *end = cells[y - 1][x].get();
+        micropather::StateCost stateCost{};
+        stateCost.state = end;
+        stateCost.cost = 1;
+        adjacent->push_back(stateCost);
+      }
+      if (start->west) {
+        auto *end = cells[y][x - 1].get();
+        micropather::StateCost stateCost{};
+        stateCost.state = end;
+        stateCost.cost = 1;
+        adjacent->push_back(stateCost);
+      }
+      if (start->east) {
+        auto *end = cells[y][x + 1].get();
+        micropather::StateCost stateCost{};
+        stateCost.state = end;
+        stateCost.cost = 1;
+        adjacent->push_back(stateCost);
+      }
+      if (start->south) {
+        auto *end = cells[y + 1][x].get();
+        micropather::StateCost stateCost{};
+        stateCost.state = end;
+        stateCost.cost = 1;
+        adjacent->push_back(stateCost);
+      }
+    }
+
+    void PrintStateInfo(void *state) override {
+      std::cout << *((cell_t *) state);
     }
 
     bool is_valid(int x, int y) const {
@@ -188,15 +253,15 @@ namespace day20 {
           }
           // Mark cells' doors
           switch (move_node->direction) {
-            case 'N': { cells[sy][sx].north = true; cells[ty][tx].south = true; break; }
-            case 'S': { cells[sy][sx].south = true; cells[ty][tx].north = true; break; }
-            case 'E': { cells[sy][sx].east = true;  cells[ty][tx].west = true;  break; }
-            case 'W': { cells[sy][sx].west = true;  cells[ty][tx].east = true;  break; }
+            case 'N': { cells[sy][sx]->north = true; cells[ty][tx]->south = true; break; }
+            case 'S': { cells[sy][sx]->south = true; cells[ty][tx]->north = true; break; }
+            case 'E': { cells[sy][sx]->east = true;  cells[ty][tx]->west = true;  break; }
+            case 'W': { cells[sy][sx]->west = true;  cells[ty][tx]->east = true;  break; }
             default: { assert(0); }
           }
           // if (trace1) std::cout << "Marking (" << sx << "," << sy << ") and (" << tx << "," << ty << ")" << std::endl;
-          cells[sy][sx].valid = true;
-          cells[ty][tx].valid = true;
+          cells[sy][sx]->valid = true;
+          cells[ty][tx]->valid = true;
           sx = tx;
           sy = ty;
         } else {
@@ -213,6 +278,52 @@ namespace day20 {
       process_path(start_x, start_y, regex.start_path);
     }
 
+    int get_most_doors_to_room() {
+      auto *start_cell = cells[start_y][start_x].get();
+      float max_cost = 0;
+      for (auto &row : cells) {
+        for (auto &end_cell : row) {
+          if (end_cell->valid) {
+            std::vector<void *> path;
+            float total_cost;
+            int result = pather->Solve(start_cell, end_cell.get(), &path, &total_cost);
+            if (result != micropather::MicroPather::NO_SOLUTION) {
+              if (result == micropather::MicroPather::START_END_SAME) {
+                total_cost = 0;
+              }
+              if (total_cost > max_cost) {
+                max_cost = total_cost;
+              }
+            }
+          }
+        }
+      }
+      return (int)max_cost;
+    }
+
+    int get_rooms_that_pass_1000_doors() {
+      auto *start_cell = cells[start_y][start_x].get();
+      int num_rooms = 0;
+      for (auto &row : cells) {
+        for (auto &end_cell : row) {
+          if (end_cell->valid) {
+            std::vector<void *> path;
+            float total_cost;
+            int result = pather->Solve(start_cell, end_cell.get(), &path, &total_cost);
+            if (result != micropather::MicroPather::NO_SOLUTION) {
+              if (result == micropather::MicroPather::START_END_SAME) {
+                total_cost = 0;
+              }
+              if (total_cost >= 1000) {
+                num_rooms++;
+              }
+            }
+          }
+        }
+      }
+      return num_rooms;
+    }
+
     friend std::ostream &operator<<(std::ostream &out, const map_t &regex) {
       // Top border
       std::cout << "#";
@@ -225,14 +336,14 @@ namespace day20 {
         // cell row
         std::cout << '#';
         for (int x = regex.bx0; x <= regex.bx1; x++) {
-          auto &doors = regex.cells[y][x];
-          if (doors.valid) {
+          auto &cell = regex.cells[y][x];
+          if (cell->valid) {
             if (regex.start_x == x && regex.start_y == y) {
               std::cout << 'X';
             } else {
               std::cout << '.';
             }
-            if (doors.east) {
+            if (cell->east) {
               std::cout << '|';
             } else {
               std::cout << '#';
@@ -246,7 +357,7 @@ namespace day20 {
         std::cout << '#';
         for (int x = regex.bx0; x <= regex.bx1; x++) {
           auto &doors = regex.cells[y][x];
-          if (doors.south) {
+          if (doors->south) {
             std::cout << '-';
           } else {
             std::cout << '#';
@@ -278,6 +389,7 @@ namespace day20 {
         map.initialize();
         map.process(regex);
         std::cout << map << std::endl;
+        assert(map.get_most_doors_to_room() == 10);
       }
       {
         regex_t regex;
@@ -287,6 +399,7 @@ namespace day20 {
         map.initialize();
         map.process(regex);
         std::cout << map << std::endl;
+        assert(map.get_most_doors_to_room() == 18);
       }
     }
 
@@ -296,7 +409,8 @@ namespace day20 {
     map_t<200> map;
     map.initialize();
     map.process(regex);
-    std::cout << map << std::endl;
+//    std::cout << map << std::endl;
+    std::cout << "Result: " << map.get_most_doors_to_room() << std::endl;
 
 #endif
   }
@@ -305,6 +419,14 @@ namespace day20 {
     std::cout << "Day 20 - Problem 2" << std::endl;
 #if !defined(ONLY_ACTIVATE) || ONLY_ACTIVATE == 20
 
+    regex_t regex;
+    read_day20_data(regex, "data/day20/problem1/input.txt");
+//    std::cout << regex << std::endl;
+    map_t<200> map;
+    map.initialize();
+    map.process(regex);
+//    std::cout << map << std::endl;
+    std::cout << "Result: " << map.get_rooms_that_pass_1000_doors() << std::endl;
 
 #endif
   }
