733606b06d72748d8767f84c3048bef7b0f5738a
Prasad Silva <prasad@bitsemble.com>
2018-12-25 09:36:56 -0800

    Day 23 problem1

--- src/day23.cpp
+++ src/day23.cpp
@@ -29,7 +29,7 @@ namespace day23 {
       assert(!line.empty());
 
       // pos=<0,0,0>, r=4
-      std::regex scan_pattern(R"(pos=<(\d+),(\d+),(\d+)>, r=(\d+))");
+      std::regex scan_pattern(R"(pos=<([-\d]+),([-\d]+),([-\d]+)>, r=(\d+))");
       std::smatch scan_matches;
       if (!std::regex_search(line, scan_matches, scan_pattern)) {
         throw new std::invalid_argument("Cannot parse nanobot!");
@@ -53,10 +53,10 @@ namespace day23 {
     }
   };
 
-  struct cell_t {
-    val_t x, y, z;
-    std::shared_ptr<nanobot_t> nanobot = nullptr;
-  };
+//  struct cell_t {
+//    val_t x, y, z;
+//    std::shared_ptr<nanobot_t> nanobot = nullptr;
+//  };
 
   val_t compute_manhattan_distance(val_t x1, val_t y1, val_t z1, val_t x2, val_t y2, val_t z2) {
     return std::abs(x1 - x2) + std::abs(y1 - y2) + std::abs(z1 - z2);
@@ -65,37 +65,37 @@ namespace day23 {
   struct env_t {
     val_t max_x = 0, max_y = 0, max_z = 0;
     std::vector<std::shared_ptr<nanobot_t>> nanobots;
-    std::vector<std::vector<std::vector<cell_t>>> cells;
+//    std::vector<std::vector<std::vector<cell_t>>> cells;
 
     env_t(const std::vector<nanobot_t> &_nanobots) {
-      // Find max dims
-      for (auto &nanobot : _nanobots) {
-        if (nanobot.x > max_x) max_x = nanobot.x;
-        if (nanobot.y > max_y) max_y = nanobot.y;
-        if (nanobot.z > max_z) max_z = nanobot.z;
-      }
-      // Reserve cells
-      cells.resize(static_cast<unsigned long>(max_z + 1));
-      for (val_t z = 0; z <= max_z; z++) {
-        auto &slice = cells[z];
-        slice.resize(static_cast<unsigned long>(max_y + 1));
-        for (val_t y = 0; y <= max_y; y++) {
-          auto &row = slice[y];
-          row.resize(static_cast<unsigned long>(max_x + 1));
-          for (val_t x = 0; x <= max_x; x++) {
-            auto &cell = row[x];
-            cell.x = x;
-            cell.y = y;
-            cell.z = z;
-          }
-        }
-      }
-      // Add nanobots to cells
+//      // Find max dims
+//      for (auto &nanobot : _nanobots) {
+//        if (nanobot.x > max_x) max_x = nanobot.x;
+//        if (nanobot.y > max_y) max_y = nanobot.y;
+//        if (nanobot.z > max_z) max_z = nanobot.z;
+//      }
+//      // Reserve cells
+//      cells.resize(static_cast<unsigned long>(max_z + 1));
+//      for (val_t z = 0; z <= max_z; z++) {
+//        auto &slice = cells[z];
+//        slice.resize(static_cast<unsigned long>(max_y + 1));
+//        for (val_t y = 0; y <= max_y; y++) {
+//          auto &row = slice[y];
+//          row.resize(static_cast<unsigned long>(max_x + 1));
+//          for (val_t x = 0; x <= max_x; x++) {
+//            auto &cell = row[x];
+//            cell.x = x;
+//            cell.y = y;
+//            cell.z = z;
+//          }
+//        }
+//      }
+//      // Add nanobots to cells
       for (auto &_nanobot : _nanobots) {
         auto nanobot = std::make_shared<nanobot_t>();
         *nanobot = _nanobot;
         nanobots.push_back(nanobot);
-        cells[_nanobot.z][_nanobot.y][_nanobot.x].nanobot = nanobot;
+//        cells[_nanobot.z][_nanobot.y][_nanobot.x].nanobot = nanobot;
       }
     }
 
@@ -149,6 +149,12 @@ namespace day23 {
     }
   }
 
+    std::vector<nanobot_t> nanobots;
+    read_day23_data(nanobots, "data/day23/problem1/input.txt");
+    env_t env(nanobots);
+    auto nanobot = env.find_nanobot_with_largest_radius();
+    std::cout << "Result: " << env.find_nanobots_in_range_of(nanobot.get()) << std::endl;
+
 
 #endif
   }
@@ -157,6 +163,14 @@ namespace day23 {
     std::cout << "Day 23 - Problem 2" << std::endl;
 #if !defined(ONLY_ACTIVATE) || ONLY_ACTIVATE == 23
 
+    if (enable_assertions) {
+      {
+        std::vector<nanobot_t> nanobots;
+        read_day23_data(nanobots, "data/day23/problem2/test1.txt");
+        env_t env(nanobots);
+
+      }
+    }
 
 #endif
   }
